4bbf2c8cd2cf7ce7b2542aec44bb68a60102d529
Joshua Comeau <joshwcomeau@gmail.com>
2017-01-15 21:22:55 -0500

    Remove console.log and lint one of the CDK files

--- .scripts/get_gh_pages_url.js
+++ .scripts/get_gh_pages_url.js
@@ -4,8 +4,9 @@
 // Do not modify this file.
 
 const parse = require('git-url-parse');
-var ghUrl = process.argv[2];
+
+const ghUrl = process.argv[2];
 const parsedUrl = parse(ghUrl);
 
-const ghPagesUrl = 'https://' + parsedUrl.owner + '.github.io/' + parsedUrl.name;
-console.log(ghPagesUrl);
+const ghPagesUrl = `https://${parsedUrl.owner}.github.io/${parsedUrl.name}`;
+console.info(ghPagesUrl);

c3727db8ff4815426453500ae471222b92354eaa
Joshua Comeau <joshwcomeau@gmail.com>
2017-01-21 17:46:48 -0500

    Story repair

--- src/components/Find/Find.stories.js
+++ src/components/Find/Find.stories.js
@@ -1,10 +1,21 @@
-import React, { Component } from 'react';
+/* eslint-disable react/prop-types */
+import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 
 import Find from '../Find';
+import Sort from '../Sort';
 
 
 storiesOf('Find', module)
-  .add('default', () => (
-    <Find />
-  ))
+  .add('default (finds the first match)', () => (
+    <Find
+      collection={[
+        { id: 'a', name: 'apple', inStock: false },
+        { id: 'b', name: 'banana', inStock: true },
+        { id: 'c', name: 'carrot', inStock: true },
+      ]}
+      predicate={item => item.inStock}
+    >
+      {item => <div key={item.id}>{item.name}</div>}
+    </Find>
+  ));
--- src/components/Last/Last.stories.js
+++ src/components/Last/Last.stories.js
@@ -1,10 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 
 import Last from '../Last';
+import Sort from '../Sort';
 
 
 storiesOf('Last', module)
-  .add('default', () => (
-    <Last />
+  .add('default (1 item)', () => (
+    <Last collection={['Apple', 'Banana', 'Carrot']}>
+      {item => <div>{item}</div>}
+    </Last>
   ))
+  .add('last 3 of 4 items', () => (
+    <Last collection={['Apple', 'Banana', 'Carrot', 'Dragonfruit']} num={3}>
+      {item => <div>{item}</div>}
+    </Last>
+  ))
+  .add('composed with Sort', () => {
+    const collection = [
+      { id: 'a', name: 'Apple', price: 5 },
+      { id: 'b', name: 'Banana', price: 10.25 },
+      { id: 'c', name: 'Carrot', price: 4.50 },
+      { id: 'd', name: 'Dragonfruit', price: 7.50 },
+      { id: 'e', name: 'Eggplant', price: 12.75 },
+    ];
+
+    return (
+      <Sort collection={collection} comparator="price">
+        <Last num={2}>
+          {item => <div>{item.name} - {item.price}</div>}
+        </Last>
+      </Sort>
+    );
+  });

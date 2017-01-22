40b6ae7944753f3fb5f0d995632e3c50f7678b0d
Joshua Comeau <joshwcomeau@gmail.com>
2017-01-22 08:41:38 -0500

    Demo copy stuff

--- demo/src/components/App/App.js
+++ demo/src/components/App/App.js
@@ -48,7 +48,7 @@ const App = ({ children }) => (
 
       <h4>See It In Action</h4>
       <p>
-        Let’s say we’re building a chat application. We’ve got the following list of users:
+        Let’s say we’re building a chat application, and we want to show a list of users we can message. We’ve got the following data:
       </p>
 
       <FiddleEmbed fiddleId="f8eyvge9" panes={['js']} revision={4} />
@@ -57,16 +57,20 @@ const App = ({ children }) => (
         The first thing we might want to do is filter the list so we only show users who are online. For this, we’ll need our friend, the <ComponentName>Filter</ComponentName> component:
       </p>
 
-      <FiddleEmbed fiddleId="465b0egv" height={225} revision={6} />
+      <FiddleEmbed fiddleId="465b0egv" height={225} revision={6}>
+        Click the 'Result' pane to see the rendered list.
+      </FiddleEmbed>
 
-      <p>Our <ComponentName>Filter</ComponentName> takes 3 params:</p>
+      <p>
+        Our <ComponentName>Filter</ComponentName> is using the following params:
+      </p>
 
       <ol>
         <li>
           <strong className="mono">collection</strong> - the array of items we want to render
         </li>
         <li>
-          <strong className="mono">predicate</strong> - a matcher that will determine whether each item will be accepted or rejected. Typically this is defined as a {'function'} that accepts the item as an argument, but here we’re using the convenient object shorthand.
+          <strong className="mono">predicate</strong> - a matcher that will determine whether each item will be accepted or rejected. You can pass it a {'function'}, similar to <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter">Array#filter</a>, but in this case we’re using the convenient object shorthand format.
         </li>
         <li>
           <strong className="mono">children</strong> - a {'function'} that decides how to render items that make it through the filter predicate. In this case, we’re rendering a User component for each accepted item.
@@ -74,9 +78,15 @@ const App = ({ children }) => (
       </ol>
 
       <p>
-        By clicking the 'Result' pane on the code snippet above, you can see that we get a nice list of our currently-online users. The list is rendered in an arbitrary order, though; it’d be nice if it was sorted by the 'lastMessagedAt' property.
+        Passing a {'function'} as children is a little unconventional, but it’s a powerful way of giving the developer more control over the children’s output. In our case, you describe how you’d like a <em>single child</em> to look, and we apply that {'function'} to every item in the collection that makes it through the filter.
       </p>
 
+
+      <h4>Composing</h4>
+
+      <p>
+        So we’ve filtered out offline users, but our list is currently being rendered in an arbitrary order. It’d make sense to sort it based on when the current user last spoke to each of their contacts.
+      </p>
       <p>
         Thankfully, <strong>React Collection Helpers are composable</strong>. Here’s how we do it, using a <ComponentName>Sort</ComponentName> component:
       </p>
@@ -84,13 +94,10 @@ const App = ({ children }) => (
       <FiddleEmbed fiddleId="d3da14hy" height={265} revision={4} />
 
       <p>
-        In the author’s humble opinion, it’s here that this set of utility components goes from a novelty to something genuinely useful.
-      </p>
-      <p>
         Nested components have the collections passed through automatically, and just need to define the attributes needed to modify it.
       </p>
       <p>
-        The <ComponentName>Sort</ComponentName> component accepts the following props:
+        The <ComponentName>Sort</ComponentName> component is using the following props:
       </p>
 
       <ol>
@@ -98,7 +105,7 @@ const App = ({ children }) => (
           <strong className="mono">collection</strong> - supplied automatically by the parent <ComponentName>Filter</ComponentName> component.
         </li>
         <li>
-          <strong className="mono">comparator</strong> - a matcher that will determine how to sort the collection. Typically, you would pass it a {'function'}, similar to how <a href="https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/sort">Array#sort</a> works. In this case, however, we’ve supplied a string shorthand, specifying which property we want to sort by.
+          <strong className="mono">comparator</strong> - a matcher that will determine how to sort the collection. It accepts a {'function'}, similar to how <a href="https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/sort">Array#sort</a> works. In this case, however, we’ve supplied a string shorthand, specifying which property we want to sort by.
         </li>
         <li>
           <strong className="mono">descending</strong> - by default, when using the 'string' shorthand for <span className="mono">comparator</span>, it will be sorted in ascending order. In this case, we want it sorted in descending order. The <span className="mono">descending</span> boolean prop allows us to make this distinction.
@@ -112,16 +119,21 @@ const App = ({ children }) => (
       </p>
 
       <p>
-        As of the time of writing, there are 10 helper components in total. The examples in this demo are just a sample of what can currently be done.
+        As of the time of writing, there are 10 helper components, and they all compose bi-directionally with each other. The examples in this demo are just a sample of what can currently be done.
       </p>
 
       <p>
         That said, React Collection Helpers is still in its infancy, and I’m still learning how it can best solve common problems. I suspect we’re still only scratching the surface of what this can be used for.</p>
 
-      <p>
+      <HorizontalRule />
+
+      <p className="contact-info centered">
         Have feedback? Ideas? Criticism? Want to help build this?
         {' '}
-        <a href="https://github.com/joshwcomeau/react-collection-helpers">Let’s talk.</a>
+        <br />
+        I’d love to talk about it.
+        {' '}
+        <a href="https://github.com/joshwcomeau/react-collection-helpers" className="contact-info">Check out the repo</a>, or <a href="https://twitter.com/JoshWComeau" className="contact-info">hit me up on Twitter</a>.
       </p>
     </MaxWidthWrapper>
   </div>
